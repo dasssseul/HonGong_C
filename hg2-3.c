4162a07d692369072c884ca31a5cc50f4f5e36ca
dasssseul <77623643+dasssseul@users.noreply.github.com>
2021-02-16 20:25:20 +0900

    Add files via upload

---
+++ hg2-3.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+/* 2-3. ¿¹Á¦ */
+int main2(void)
+{
+	printf("Be happy\n");
+	printf("12345678901234567890\n");
+	printf("My\t friend\n");
+	printf("Goot\bd chance\n");
+	printf("Cow\rW\a\n");
+
+	return 0;
+}
